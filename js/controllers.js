e21630d3c1f27ecc9ea8cb6777dd31b05923fd95
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2014-06-11 17:28:56 -0400

    init

---
+++ js/controllers.js
@@ -0,0 +1,37 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function() {
+
+  var expresions = [
+    "Wow, you are so smart!",
+    "You wanna medal !!!",
+    "I like squash! Do you???",
+    "Cool story bro!",
+    "I think it's gonna rain today!",
+    "How about that local sports team?",
+    "Was your mother proud of you?",
+    "You should tell your mom",
+    "There's an app for that",
+    "Hold on, let me refer to my social dictionary!",
+    "Let's make an app for that!",
+    "Are you a genius?"
+  ];
+
+  var word;
+
+  var random = function() {
+    var index = Math.floor((Math.random() * (expresions.length - 1)));
+    word = expresions[index];
+  };
+
+  var getWord = function() {
+    return word;
+  };
+
+  return {
+    random : random,
+    word: getWord
+  }
+  
+})
+
