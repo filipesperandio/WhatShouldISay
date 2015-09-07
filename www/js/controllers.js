61ba38cdad6edb174b5373cce2251236ba06a084
Filipe Esperandio <filipesperandio@gmail.com>
2015-09-07 15:41:34 -0300

    spinner when loading data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,32 @@
+function LoadingSpinner () {
+  var opts = {
+    lines: 9 // The number of lines to draw
+    , length: 15 // The length of each line
+    , width: 8 // The line thickness
+    , radius: 15 // The radius of the inner circle
+    , scale: 1 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.25 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+  };
+  var _target = document.getElementById('spinner');
+  var spinner = new Spinner(opts).spin(_target);
+
+  return spinner;
+};
+
 var Expressions = function($firebase, FIREBASE_URL) {
   var ref = new Firebase(FIREBASE_URL + '/expressions1');
   return $firebase(ref);
@@ -20,11 +49,13 @@ var Storage = function() {
 };
 
 var DashCtrl = function(Expressions, Storage, $timeout) {
+  var spinner = LoadingSpinner();
 
   Expressions.$on('loaded', function() {
     saveAll();
     random();
     Expressions.$on('change', saveAll);
+    spinner.stop();
   });
 
   var saveAll = function() {
@@ -43,6 +74,7 @@ var DashCtrl = function(Expressions, Storage, $timeout) {
 
   var once = false;
   var getWord = function() {
+    console.log("...", word);
     return word;
   };
 
